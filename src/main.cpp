8fb4eb50e0c01a6343e0789e1d1a46ddea1e993e
Sergio Portolés <Sergio.PortolesDiez@mech.kuleuven.be>
2013-03-27 14:45:22 +0100

    Started the project including the source folder /src Inside source folder src/main.cpp prints an spanish hello-world
    Also added the folders .cproject and .project generated by
    Eclipse

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 27 Mar 2013
+ *      Author: Sergio Portoles
+ */
+
+#include <cstdio>
+#include <iostream>
+
+int main (int argc, char *argv[]) {
+
+	// Main output of the program
+	std::cout << "Hola mundo!" << std::endl;
+	return 0;
+}
+
